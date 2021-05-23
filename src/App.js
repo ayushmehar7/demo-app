3c6766904aa066ff2fc5fcea2bf82d54ec1a8d79
Ayush Mehar <ayushm19100@iiitnr.edu.in>
2021-05-23 22:29:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      pressed : 0
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        <h1>Welcome to Our Demo App</h1>
+        <button className = "button" onClick = {()=>{
+          this.setState({pressed : this.state.pressed+1})
+        }}>Click Here</button>
+        <h2>Button Pressed : {this.state.pressed} Times!</h2>
+      </div>
+    )
+  }
 }
 
 export default App;
